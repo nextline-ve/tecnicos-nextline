14068d877fe402443b7ab23d414e5523ec27b79d
abejarano <programador.angel@gmail.com>
2020-11-09 22:06:17 -0300

    package name.

--- lib/widgets/image_viewer.dart
+++ lib/widgets/image_viewer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nextline/utils/app_colors.dart';
+import 'package:tenicos_nextline/utils/app_colors.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:tenicos_nextline/utils/app_colors.dart';
 
