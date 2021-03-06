62b6e16403baad424ff9139eba01b4c008954dba
abejarano <programador.angel@gmail.com>
2021-03-05 22:47:20 -0300

    mejorando la estructura de codigo mas boton flotante

--- lib/Technician/Assignment/ui/screens/success_repair_screen.dart
+++ lib/Technician/Assignment/ui/screens/success_repair_screen.dart
@@ -34,7 +34,6 @@ class _SuccessRepairScreen extends State<SuccessRepairScreen> {
 
   void getImage(ImageSource source) {
     picker.getImage(source: source).then((PickedFile pickedFile) async {
-      print(pickedFile);
       setState(() {
         pickedFile.readAsBytes().then((value) => _uploadedFiles.add(value));
       });
@@ -53,179 +52,178 @@ class _SuccessRepairScreen extends State<SuccessRepairScreen> {
             style: TextStyle(fontFamily: AppFonts.input, fontSize: 16),
           ),
         ),
-        body: Stack(children: [
-          Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              alignment: Alignment.center,
-              color: Colors.white,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          "Reparación de Avería",
-                          style: TextStyle(
-                              color: AppColors.blue_dark,
-                              fontSize: 18,
-                              fontFamily: AppFonts.poppins_bold),
-                        )),
-                    Expanded(
-                      child: ListView(
-                        scrollDirection: Axis.vertical,
-                        children: [
-                          Column(children: [
-                            InputContainer(
-                              label: "Trabajo Realizado",
-                              padding: EdgeInsets.only(
-                                  left: 0, right: 0, bottom: 24),
-                              input: TextFormField(
-                                maxLines: 8,
-                                decoration: InputDecoration.collapsed(
-                                    hintText: "Describa el trabajo realizado",
-                                    hintStyle: TextStyle(
-                                        color: AppColors.black_color
-                                            .withOpacity(0.34),
-                                        fontSize: 12,
-                                        fontFamily: AppFonts.poppins_regular)),
-                                onChanged: (val) => _observacion = val,
-                              ),
+        body: bodyWindow(),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Agregar material',
+        onPressed: () {
+          setState(() {
+            _materialUsado.add({"value": "", "quantity": 0});
+          });
+        },
+        child: Icon(Icons.library_add_check),
+        backgroundColor: AppColors.blue_dark,
+      ),
+    );
+
+
+  }
+
+  Widget bodyWindow() {
+    return Stack(children: [
+      Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          alignment: Alignment.center,
+          color: Colors.white,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "Reparación de Avería",
+                      style: TextStyle(
+                          color: AppColors.blue_dark,
+                          fontSize: 18,
+                          fontFamily: AppFonts.poppins_bold),
+                    )),
+                Expanded(
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    children: [
+                      Column(children: [
+                        InputContainer(
+                          label: "Trabajo Realizado",
+                          padding: EdgeInsets.only(
+                              left: 0, right: 0, bottom: 24),
+                          input: TextFormField(
+                            maxLines: 8,
+                            decoration: InputDecoration.collapsed(
+                                hintText: "Describa el trabajo realizado",
+                                hintStyle: TextStyle(
+                                    color: AppColors.black_color
+                                        .withOpacity(0.34),
+                                    fontSize: 12,
+                                    fontFamily: AppFonts.poppins_regular)),
+                            onChanged: (val) => _observacion = val,
+                          ),
+                        ),
+                        Container(
+                          width: 250,
+                          child: JButton(
+                            padding: EdgeInsets.all(10),
+                            icon: Icons.camera_alt,
+                            isIconRight: true,
+                            fontSize: 10,
+                            labelColor: AppColors.white_color,
+                            label: "Subir Foto",
+                            background: AppColors.blue,
+                            buttonHeight: 40.0,
+                            onTab: () => {
+                              showMyDialog(
+                                  context, "Seleccionar una foto", getImage)
+                            },
+                          ),
+                        ),
+                        if (_uploadedFiles.length > 0)
+                          Container(
+                            height: 150,
+                            child: ListView(
+                              shrinkWrap: true,
+                              scrollDirection: Axis.horizontal,
+                              children: _uploadedFiles
+                                  .map((file) => Image.memory(file))
+                                  .toList(),
                             ),
-                            Container(
-                              width: 250,
-                              child: JButton(
-                                padding: EdgeInsets.all(10),
-                                icon: Icons.camera_alt,
-                                isIconRight: true,
-                                fontSize: 10,
-                                labelColor: AppColors.white_color,
-                                label: "Subir Foto",
-                                background: AppColors.blue,
-                                buttonHeight: 40.0,
-                                onTab: () => {
-                                  showMyDialog(
-                                      context, "Seleccionar una foto", getImage)
-                                },
-                              ),
+                          ),
+                        if (_materialUsado.length > 0)
+                          Container(
+                            child: ListView(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              children: _materialUsado
+                                  .map(
+                                    (mat) => formMaterial(),
+                              )
+                                  .toList(),
                             ),
-                            if (_uploadedFiles.length > 0)
-                              Container(
-                                height: 150,
-                                child: ListView(
-                                  shrinkWrap: true,
-                                  scrollDirection: Axis.horizontal,
-                                  children: _uploadedFiles
-                                      .map((file) => Image.memory(file))
-                                      .toList(),
-                                ),
-                              ),
-                            if (_materialUsado.length > 0)
-                              Container(
-                                height: 300,
-                                child: ListView(
-                                  children: _materialUsado
-                                      .map(
-                                        (mat) => Row(children: [
-                                          Expanded(
-                                            flex: 2,
-                                            child: InputContainer(
-                                                padding: EdgeInsets.only(
-                                                    left: 24,
-                                                    right: 10,
-                                                    bottom: 24),
-                                                label: "Material Utilizado",
-                                                input: DropdownWidget(
-                                                  hintText:
-                                                      "Seleccione un material",
-                                                  options: [],
-                                                )),
-                                          ),
-                                          Expanded(
-                                            flex: 1,
-                                            child: InputContainer(
-                                              padding: EdgeInsets.only(
-                                                  left: 10,
-                                                  right: 24,
-                                                  bottom: 24),
-                                              label: "Cantidad",
-                                              input: TextFormField(
-                                                keyboardType:
-                                                    TextInputType.number,
-                                                decoration:
-                                                    InputDecoration.collapsed(
-                                                        hintText: "0",
-                                                        hintStyle: TextStyle(
-                                                            color: AppColors
-                                                                .black_color
-                                                                .withOpacity(
-                                                                    0.34),
-                                                            fontSize: 18,
-                                                            height: 2.8,
-                                                            fontFamily: AppFonts
-                                                                .poppins_regular)),
-                                              ),
-                                            ),
-                                          ),
-                                        ]),
-                                      )
-                                      .toList(),
-                                ),
-                              ),
-                            // Row(
-                            //   mainAxisAlignment: MainAxisAlignment.end,
-                            //   children: [
-                            //     GestureDetector(
-                            //         onTap: () {
-                            //           setState(() {
-                            //             _materialUsado
-                            //                 .add({"value": "", "quantity": 0});
-                            //           });
-                            //         },
-                            //         child: Container(
-                            //             padding: EdgeInsets.all(10),
-                            //             child: Icon(
-                            //               Icons.add,
-                            //               color: Colors.white,
-                            //             ),
-                            //             decoration: BoxDecoration(
-                            //               color: AppColors.blue_dark,
-                            //               borderRadius:
-                            //                   BorderRadius.circular(10),
-                            //             ))),
-                            //   ],
-                            // ),
-                          ]),
-                        ],
-                      ),
-                    ),
-                    JButton(
-                      label: "CONFIRMACIÓN DEL CLIENTE",
-                      padding: EdgeInsets.symmetric(vertical: 20),
-                      background: AppColors.green_color,
-                      onTab: () {
-                        if (_uploadedFiles.length != 4) {
-                          return;
-                        }
-                        widget.assignment.observacion = this._observacion;
-                        widget.assignment.fotoUno =
-                            base64Encode(this._uploadedFiles[0]);
-                        widget.assignment.fotoDos =
-                            base64Encode(this._uploadedFiles[1]);
-                        widget.assignment.fotoTres =
-                            base64Encode(this._uploadedFiles[2]);
-                        widget.assignment.fotoCuatro =
-                            base64Encode(this._uploadedFiles[3]);
-                        widget.assignment.realizado = true;
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ClientConfirmationScreen(
-                                      assignment: widget.assignment,
-                                      blocTickets: widget.blocTickets,
-                                    )));
-                      },
-                    ),
-                  ]))
-        ]));
+                          ),
+                      ]),
+                    ],
+                  ),
+                ),
+                JButton(
+                  label: "CONFIRMACIÓN DEL CLIENTE",
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  background: AppColors.green_color,
+                  onTab: () {
+                    if (_uploadedFiles.length != 4) {
+                      return;
+                    }
+                    widget.assignment.observacion = this._observacion;
+                    widget.assignment.fotoUno =
+                        base64Encode(this._uploadedFiles[0]);
+                    widget.assignment.fotoDos =
+                        base64Encode(this._uploadedFiles[1]);
+                    widget.assignment.fotoTres =
+                        base64Encode(this._uploadedFiles[2]);
+                    widget.assignment.fotoCuatro =
+                        base64Encode(this._uploadedFiles[3]);
+                    widget.assignment.realizado = true;
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ClientConfirmationScreen(
+                              assignment: widget.assignment,
+                              blocTickets: widget.blocTickets,
+                            )));
+                  },
+                ),
+              ]))
+    ]);
+  }
+
+  Widget formMaterial() {
+    return Row(children: [
+      Expanded(
+        flex: 2,
+        child: InputContainer(
+            padding: EdgeInsets.only(
+                left: 24,
+                right: 10,
+                bottom: 24),
+            label: "Material Utilizado",
+            input: DropdownWidget(
+              hintText:
+              "Seleccione un material",
+              options: [],
+            )),
+      ),
+      Expanded(
+        flex: 1,
+        child: InputContainer(
+          padding: EdgeInsets.only(
+              left: 10,
+              right: 24,
+              bottom: 24),
+          label: "Cantidad",
+          input: TextFormField(
+            keyboardType:
+            TextInputType.number,
+            decoration:
+            InputDecoration.collapsed(
+                hintText: "0",
+                hintStyle: TextStyle(
+                    color: AppColors
+                        .black_color
+                        .withOpacity(
+                        0.34),
+                    fontSize: 18,
+                    height: 2.8,
+                    fontFamily: AppFonts
+                        .poppins_regular)),
+          ),
+        ),
+      ),
+    ]);
   }
 }
